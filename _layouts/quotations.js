66f42a4dd7f44aaf91c0cb176226f5b52f1c0513
Rylan <rylanschaeffer@gmail.com>
2020-06-19 21:31:01 -0700

    trying to add random quote sampling

---
+++ _layouts/quotations.js
@@ -0,0 +1,58 @@
+const quotations = [
+    "So we seem to find, in our selves, unity over time, unity of function, unity in space. Yet" +
+    "each of these unities is contingent. In pathology or in thought experiment, the integrity of" +
+    "our memories can be compromised, the connectivity of our brains can be split or fragmented," +
+    "and our bodies can be broken apart and rebuilt. What then am I?",
+
+    "We have to go. I'm almost happy here. I've lived too long with pain. I won’t know who I am without it.",
+
+    "Humanity does not ask us to be happy. It merely asks us to be brilliant on its behalf.",
+
+    "If we're not in pain, we're not alive.",
+
+    "But that, that distance — that chronic sense of being an alien among your own kind — it's not entirely a bad thing.",
+
+    "Today you... tomorrow me.",
+
+    "We all make choices, but in the end, our choices make us.",
+
+    "Yet, he thought, if I can die saying 'Life is so beautiful,' then nothing else is important.",
+
+    "I know I can be what I want to be; if I work hard at it, I'll be what I want to be.",
+
+    "For you are life, rarer than a quark and unpredictable beyond the dreams of Heisenberg.",
+
+    "Our freedom to doubt was born of a struggle against authority in the early days of science. It was" +
+    "a very deep and strong struggle. Permit us to question - to doubt, that's all - not to be sure.",
+
+    "It measures everything, in short, except that which makes life worthwhile. And it can us everything" +
+    "about America, except why we are proud that we are Americans.",
+
+    "It never gets easier. You just go faster.",
+
+    "A lesson without pain is meaningless, for you cannot gain anything without sacrificing something" +
+    "else in return. But once you have overcome it and made it your own, you will gain an irreplaceable" +
+    "fullmetal heart.",
+
+    "I want you to feel what I felt. I want you to know why a story-truth is truer sometimes" +
+    "than happening truth.",
+
+    "Someone once told me the definition of hell: the last day you have on earth, the person " +
+    "you became meets the person you could have become.",
+
+    "We all need mirrors to remind ourselves who we are. I’m no different.",
+
+    "You be good. I love you.",
+
+    "Do not wait for the Last Judgment. It takes place every day.",
+
+    "Sunday's my favorite day again. I think of what everyone did for me, and I feel like a very lucky guy.",
+
+    "Champion is not just a noun.",
+
+    "Rage, my soldiers. Scream, my soldiers.",
+
+    "That's why the people who push themselves into hell see a different hell from the rest of us.",
+
+    ""
+];
