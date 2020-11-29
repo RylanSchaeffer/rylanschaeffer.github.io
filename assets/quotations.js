68f58dc031b1ed7d116042215c84fb2a26d7d119
Rylan <rylanschaeffer@gmail.com>
2020-11-28 22:22:29 -0800

    Added quotation marks to quotations

--- assets/quotations.js
+++ assets/quotations.js
@@ -1,116 +1,114 @@
 const quotations = [
-    "So we seem to find, in our selves, unity over time, unity of function, unity in space. Yet " +
+    "“So we seem to find, in our selves, unity over time, unity of function, unity in space. Yet " +
     "each of these unities is contingent. In pathology or in thought experiment, the integrity of" +
     "our memories can be compromised, the connectivity of our brains can be split or fragmented, " +
-    "and our bodies can be broken apart and rebuilt. What then am I?",
+    "and our bodies can be broken apart and rebuilt. What then am I?“",
 
-    "We have to go. I'm almost happy here. I've lived too long with pain. I won’t know who I am without it.",
+    "“We have to go. I'm almost happy here. I've lived too long with pain. I won’t know who I am without it.“",
 
-    "Humanity does not ask us to be happy. It merely asks us to be brilliant on its behalf.",
+    "“Humanity does not ask us to be happy. It merely asks us to be brilliant on its behalf.“",
 
-    "If we're not in pain, we're not alive.",
+    "“If we're not in pain, we're not alive.“",
 
-    "But that, that distance — that chronic sense of being an alien among your own kind — it's not entirely a bad thing.",
+    "“But that, that distance — that chronic sense of being an alien among your own kind — it's not entirely a bad thing.“",
 
-    "Today you... tomorrow me.",
+    "“Today you... tomorrow me.“",
 
-    "We all make choices, but in the end, our choices make us.",
+    "“We all make choices, but in the end, our choices make us.“",
 
-    "Yet, he thought, if I can die saying 'Life is so beautiful,' then nothing else is important.",
+    "“Yet, he thought, if I can die saying 'Life is so beautiful,' then nothing else is important.“",
 
-    "I know I can\n" +
+    "“I know I can\n" +
     "be what I want to be\n" +
     "if I work hard at it\n" +
-    "I'll be what I want to be.",
+    "I'll be what I want to be.“",
 
-    "For you are life, rarer than a quark and unpredictable beyond the dreams of Heisenberg.",
+    "“For you are life, rarer than a quark and unpredictable beyond the dreams of Heisenberg.“",
 
-    "Our freedom to doubt was born of a struggle against authority in the early days of science. It was " +
-    "a very deep and strong struggle. Permit us to question - to doubt, that's all - not to be sure.",
+    "“Our freedom to doubt was born of a struggle against authority in the early days of science. It was " +
+    "a very deep and strong struggle. Permit us to question - to doubt, that's all - not to be sure.“",
 
-    "It measures everything, in short, except that which makes life worthwhile. And it can us everything " +
-    "about America, except why we are proud that we are Americans.",
+    "“It measures everything, in short, except that which makes life worthwhile. And it can us everything " +
+    "about America, except why we are proud that we are Americans.“",
 
-    "It never gets easier. You just go faster.",
+    "“It never gets easier. You just go faster.“",
 
-    "A lesson without pain is meaningless, for you cannot gain anything without sacrificing something " +
+    "“A lesson without pain is meaningless, for you cannot gain anything without sacrificing something " +
     "else in return. But once you have overcome it and made it your own, you will gain an irreplaceable " +
-    "fullmetal heart.",
+    "fullmetal heart.“",
 
-    "I want you to feel what I felt. I want you to know why a story-truth is truer sometimes " +
-    "than happening truth.",
+    "“I want you to feel what I felt. I want you to know why a story-truth is truer sometimes " +
+    "than happening truth.“",
 
-    "Someone once told me the definition of hell: the last day you have on earth, the person " +
-    "you became meets the person you could have become.",
+    "“Someone once told me the definition of hell: the last day you have on earth, the person " +
+    "you became meets the person you could have become.“",
 
-    "We all need mirrors to remind ourselves who we are. I’m no different.",
+    "“We all need mirrors to remind ourselves who we are. I’m no different.“",
 
-    "You be good. I love you.",
+    "“You be good. I love you.“",
 
-    "Do not wait for the Last Judgment. It takes place every day.",
+    "“Do not wait for the Last Judgment. It takes place every day.“",
 
-    "Sunday's my favorite day again. I think of what everyone did for me, and I feel like a very lucky guy.",
+    "“Champion is not just a noun.“",
 
-    "Champion is not just a noun.",
+    "“Rage, my soldiers. Scream, my soldiers.“",
 
-    "Rage, my soldiers. Scream, my soldiers.",
+    "“That's why the people who push themselves into hell see a different hell from the rest of us.“",
 
-    "That's why the people who push themselves into hell see a different hell from the rest of us.",
+    "“Then one day you find ten years have got behind you, no one told you when to run, you missed the starting gun“",
 
-    "Then one day you find ten years have got behind you, no one told you when to run, you missed the starting gun",
-
-    "Time, it took the most of me\n" +
+    "“Time, it took the most of me\n" +
     "And left me with no key\n" +
-    "To unlock the chest of remedy",
+    "To unlock the chest of remedy“",
 
-    "Encumbered forever by desire and ambition\n" +
-    "There's a hunger still unsatisfied",
+    "“Encumbered forever by desire and ambition\n" +
+    "There's a hunger still unsatisfied“",
 
-    "Where were you when I was burned and broken\n" +
+    "“Where were you when I was burned and broken\n" +
     "While the days slipped by from my window watching\n" +
-    "And where were you when I was hurt and I was helpless?",
+    "And where were you when I was hurt and I was helpless?“",
 
-    "Ad astra per aspera",
+    "“Ad astra per aspera“",
 
-    "Marry someone who makes you laugh because life is hard.",
+    "“Marry someone who makes you laugh because life is hard.“",
 
-    "Because everything here must still be accomplished. It depends only on him to " +
-    "participate in this privileged act of creation.",
+    "“Because everything here must still be accomplished. It depends only on him to " +
+    "participate in this privileged act of creation.“",
 
-    "Then let me start again, I cried\n" +
+    "“Then let me start again, I cried\n" +
     "Please let me start again\n" +
     "I want a face that's fair this time\n" +
-    "I want a spirit that is calm",
+    "I want a spirit that is calm“",
 
     "\"Lonely? You have yourself. Your infinite selves. " +
     "It's a nonstop party where all the guests are the only person we like.\"",
 
-    "Don't take your eyes off them for a second. And don't ever forget them, " +
-    "because I promise that they won't forget you.",
+    "“Don't take your eyes off them for a second. And don't ever forget them, " +
+    "because I promise that they won't forget you.“",
 
-    "Structurally, there's no discernible difference. Life and death are " +
-    "unquantifiable abstracts. Why should I be concerned?",
+    "“Structurally, there's no discernible difference. Life and death are " +
+    "unquantifiable abstracts. Why should I be concerned?“",
 
     "“All we ever see of stars are their old photographs.”",
 
-    "Through my blue fingers, pink grains are falling, haphazard, random, a" +
+    "“Through my blue fingers, pink grains are falling, haphazard, random, a" +
     " disorganized stream of silicone that seems pregnant with the possibility " +
     "of every conceivable shape... But this is illusion. Things have their shape" +
-    " in time, not space alone. ",
+    " in time, not space alone.“",
 
-    "Ad majora",
+    "“Ad majora“",
 
-    "Remember, in the burning heart\n" +
-    "There can never be surrender at all.",
+    "“Remember, in the burning heart\n" +
+    "There can never be surrender at all.“",
 
-    "This is pretty lazy, seeing as you always talk such a big game. Quit laying' down " +
-    "on the job.",
+    "“This is pretty lazy, seeing as you always talk such a big game. Quit laying' down " +
+    "on the job.“",
 
-    "For the blood is running still\n" +
-    "From the roots of Yggdrasil",
+    "“For the blood is running still\n" +
+    "From the roots of Yggdrasil“",
 
-    "When the tears are in your eyes\n" +
+    "“When the tears are in your eyes\n" +
     "You sing a drunken lullaby\n" +
     "And the weight of the world\n" +
-    "Comes crashing on your spine",
+    "Comes crashing on your spine“",
 ];
